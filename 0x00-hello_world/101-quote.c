2545a395fe0b846fbcb7a483dab922af94e9eae3
Awowole Hammad <lexycon002@gmail.com>
2023-02-16 08:52:33 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that print a line without function
+ * Return:0 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n");
+	return (0);
+}
