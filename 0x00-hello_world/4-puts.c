e6c519aa281ca4eab2d5180af63af981afd0c58b
Awowole Hammad <lexycon002@gmail.com>
2023-02-16 04:08:52 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main - A program that print a line
- * return: 0 (success)
+ * Return: 0 (success)
  */
-int main(void)
+int main(void);
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
