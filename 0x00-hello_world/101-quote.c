9c855648f1d02b63b1bb80377a529e0583555dae
Awowole Hammad <lexycon002@gmail.com>
2023-02-16 08:59:28 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	fput("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n");
-	return (0);
+	fput("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n",stdout);
+	return (1);
 }
