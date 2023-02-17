b2e3d666c82a60cb46dd43140770d760b52b1dd6
Awowole Hammad <lexycon002@gmail.com>
2023-02-17 14:37:00 -0800

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - This program assigned a random number to the variable n
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is  %s\n", n, "positive");
+	}
+	else if (n < 0)
+	{
+		printf("%d is %s\n", n, "negative");
+	}
+	else
+	{
+		printf("%d is %s\n", n, "zero");
+	}
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,4 @@
+#Variable, if, else if, else statement in C programming language
+*Variable are used in storing information
+**The if, else if, else are conditional assigned statement which are been used to check the concept of a statement if it meet a certain a criteria.
+
