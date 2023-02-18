d1369adb5ead5509972f8114c8bbc705ccd02a4c
Awowole Hammad <lexycon002@gmail.com>
2023-02-17 19:54:14 -0800

    Ascending printing order

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - program that print combination of a single digit number
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar (' ');
+		}
+			++n:;
+	}
+
+	putchar('\n');
+	return (0);
+}
