c6a0de623585bcfc91d5f55d4215e5be56cecae4
Awowole Hammad <lexycon002@gmail.com>
2023-02-17 18:28:00 -0800

    Numberz base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program that prints numbers of base 10
+ *
+ * Return: 0 on Success
+ */
+
+int main(void)
+{
+	int b = '0';
+
+	while (b >= 9)
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
