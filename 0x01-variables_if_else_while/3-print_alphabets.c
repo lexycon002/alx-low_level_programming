0431e4ad3b70096d6c937d6eaf97fae586fca15f
Awowole Hammad <lexycon002@gmail.com>
2023-02-17 16:59:53 -0800

    Alphabet to uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - A program that print the Alphabet in lowercase and then uppercase
+ * Return: 0 on Success
+ */
+int main(void)
+{
+	char lc = 'a';
+	char uc = 'A';
+
+	while (lc <= 'a')
+	{
+		putchar(lc);
+		lc++;
+	}
+	while (uc = 'A')
+	{
+		putchar(uc);
+		uc++;
+	}
+	return (0);
+}
