70557ca96b3df0bb482f3ffb898ec4925a4d5d26
Awowole Hammad <lexycon002@gmail.com>
2023-02-21 12:01:31 -0800

    10x alphabet to lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - A program that prints alphabet in lowercase 10times
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char alp;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (alp = 'a'; alp <= 'z' ; alp++)
+			_putchar(alp);
+		_putchar('\n');
+	}
+}
