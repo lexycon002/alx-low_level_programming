067436fd8b0f41575008986a696bacb9f3054ed1
Awowole Hammad <lexycon002@gmail.com>
2023-02-21 07:10:28 -0800

    Alphabet to lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - A program that print the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char alph = 'a';
+
+	while (alph <= 'z')
+	{
+		_putchar(alph);
+		alph++;
+	}
+	_putchar('\n');
+	
+}
+
