a7184a0dd3b3b3fa2cecb2364faae01ea40efc6c
Awowole Hammad <lexycon002@gmail.com>
2023-02-21 16:16:24 -0800

    6 abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - program that compute the absolute value of an integer
+ *
+ * @int: function parameter
+ * Return: always 0
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (0);
+	}
+	return (-n);
+}
