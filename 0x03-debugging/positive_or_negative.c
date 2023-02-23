36c8c7e7fb465c8df186c7c629b054122aebf57b
Awowole Hammad <lexycon002@gmail.com>
2023-02-23 15:40:17 -0800

    added file leap year

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/** positive_or_negative - to check for either positive or negative
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is positive\n", i);
+}
