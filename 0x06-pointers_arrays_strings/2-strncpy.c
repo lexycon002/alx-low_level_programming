2ea8563c3ecd07e382305f6eadcbec418c453eff
Awowole Hammad <lexycon002@gmail.com>
2023-03-01 02:07:14 -0800

    copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncpy - A function that copies a string
+ * @dest: The first paramenter
+ * @src: The second parameter
+ * @n: The n parameter
+ *
+ * Return: the string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int cpy_str = 0;
+	int cha_str = 0;
+
+	while (src[cpy_str++])
+	{
+		cha_str++;
+	}
+
+	for (cpy_str = 0; src[cpy_str] && cpy_str < n; cpy_str++)
+	{
+		dest[cpy_str] = src[cha_str];
+
+	for (index = cha_str; cpy_str < n; cpy_str++)
+	{
+		dest[cpy_str] = '\0';
+	}
+	return (dest);
+}
