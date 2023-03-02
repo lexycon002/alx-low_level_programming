132507248eb40918dc5706df3d63be0ac33ecdbf
Awowole Hammad <lexycon002@gmail.com>
2023-03-02 13:12:28 -0800

    Print integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,12 +9,17 @@ void print_number(int n)
 {
 	unsigned int num = n;
 
+
+
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -num;
+		num = -n;
 	}
-	if ((num / 10) > 0)
+
+	if (num / 10 != 0)
+	{
 		print_number(num / 10);
+	}
 	_putchar((num % 10) + '0');
 }
