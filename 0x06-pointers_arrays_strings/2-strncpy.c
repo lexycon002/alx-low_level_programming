64c77fae127c1131a6d2a80fe52d5d47bf894210
Awowole Hammad <lexycon002@gmail.com>
2023-03-02 03:32:23 -0800

    Copy String

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -22,8 +22,8 @@ char *_strncpy(char *dest, char *src, int n)
 	for (cpy_str = 0; src[cpy_str] && cpy_str < n; cpy_str++)
 	{
 		dest[cpy_str] = src[cha_str];
-
-	for (index = cha_str; cpy_str < n; cpy_str++)
+	}
+	for (cpy_str = cha_str; cpy_str < n; cpy_str++)
 	{
 		dest[cpy_str] = '\0';
 	}
