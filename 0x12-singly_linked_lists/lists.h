3f708e38a2b1a55ee40591b8b31434ba64a102ac
Awowole Hammad Olamilekan <lexycon002@gmail.com>
2023-03-30 05:57:22 -0700

    header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,7 +2,14 @@
 #define LISTS_H
 
 #include <stdlib.h>
-
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct list_s
 {
 	char *str;
